267d714655ac8cc7e235ace67b922eb90e4907e6
Ankey <ankitbahuguna102@gmail.com>
2023-03-18 21:26:55 +0530

    Learning react hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+
+
+const App = () => {
+
+  const [] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>-</button>
+      <h1>0</h1>
+      <button>+</button>
     </div>
   );
 }
